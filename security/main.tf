6bacc0c1d590d5afabf539362e244976b029ae93
Satya Sahani <satyasahani7793@gmail.com>
2022-09-12 16:26:28 +0000

    Update main.tf

--- security/main.tf
+++ security/main.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "vpc" {
   config = {
     organization = "rootorg"
     workspaces = {
-      name = "terraform-code-vpc"
+      name = "terraform-code-security"
     }
   }
 }
