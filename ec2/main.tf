2b83295bec79b500796c03c7ebaa862233aaa710
Satya Sahani <satyasahani7793@gmail.com>
2022-09-13 03:57:46 +0000

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -25,28 +25,22 @@ data "terraform_remote_state" "iam-role" {
     }
   }
 }
-
-
-
 module "ec2" {
   source  = "app.terraform.io/veloce-1/ec2/aws"
   version = "1.0.2"
   # insert required variables here
 
-
-
-
- # subnet_id                       = data.terraform_remote_state.vpc.outputs.aws_subnet_publicsubnets_01_id.id
+  vpc_name                         = var.vpc_name
+  subnet_name                      = var.subnet_name
   ec2_ami_id                       = var.ec2_ami_id
   instance_type                    = var.instance_type
-  #subnet_id              = var.public_subnet
   ec2-server-name                  = var.ec2-server-name
   ec2-server-environment           = var.ec2-server-environment
   ec2-service-owner                = var.ec2-service-owner
   key_pair_name                    = var.key_pair_name
   region                           = var.region
-  #vpc                              = data.terraform_remote_state.vpc.outputs.aws_vpc_id
-  security_group                   = data.terraform_remote_state.security.outputs.aws_security_group_ssh_id.id
   iam_instance_profile             = data.terraform_remote_state.iam-role.outputs.iam_instance_profile
- # subnet_id = "Veloce_Pub_Subnet_01"
+  vpc                              = data.terraform_remote_state.vpc.outputs.aws_vpc_id
+  public_subnet  = data.terraform_remote_state.vpc.outputs.aws_subnet_publicsubnets_01_id
+  security_group = data.terraform_remote_state.security.outputs.aws_security_group_ssh_id
 }
