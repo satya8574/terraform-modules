93ab5728dffb3712e0fab1dd48c80565924fc954
Satya Sahani <satyasahani7793@gmail.com>
2022-09-13 03:37:31 +0000

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -46,6 +46,6 @@ module "ec2" {
   region                           = var.region
   #vpc                              = data.terraform_remote_state.vpc.outputs.aws_vpc_id
   security_group                   = data.terraform_remote_state.security.outputs.aws_security_group_ssh_id.id
-  iam_instance_profile             = data.terraform_remote_state.iam-role.output.iam_instance_profile
+  iam_instance_profile             = data.terraform_remote_state.iam-role.outputs.iam_instance_profile
  # subnet_id = "Veloce_Pub_Subnet_01"
 }
